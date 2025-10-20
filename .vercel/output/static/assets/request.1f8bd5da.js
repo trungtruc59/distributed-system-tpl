2edd7d43aefa7cdc52bd7f2101f333a3711c78b6
NTTruc <mobpyschi@gmail.com>
2025-10-21 00:53:48 +0700

    fix error build

---
+++ .vercel/output/static/assets/request.1f8bd5da.js
@@ -0,0 +1,1 @@
+import{j as u}from"./arco.c560203c.js";import{u as i}from"./loading.de862321.js";function d(o,s=[],t=!0){const{loading:a,setLoading:n}=i(t),e=u(s);return o().then(r=>{e.value=r.data}).finally(()=>{n(!1)}),{loading:a,response:e}}export{d as u};
